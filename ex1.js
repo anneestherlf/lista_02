1ec407ec2a7c624ebe81aca145b8d9c40f810864
Anne Esther Lins Figueirôa <anne.figueiroa@sou.inteli.edu.br>
2025-03-18 16:48:19 -0300

    inicio da atv (q1)

---
+++ ex1.js
@@ -0,0 +1,20 @@
+let p = 10;
+let q = 3;
+let r = 6;
+
+// && "and" lógico
+// || "or" lógico
+//  ! "not" lógico
+
+let resultado = (p % q === 1) && (r * 2 > p) || (q + r < p);
+console.log(resultado);
+
+// const valores = [3,6,9,12,15];
+// let produto = 1;
+
+// for (let j = 0; j < valores.length; j++) {
+//     produto *= valores[j];
+// }
+
+// console.log("O produto dos valores é: ", produto);
+
